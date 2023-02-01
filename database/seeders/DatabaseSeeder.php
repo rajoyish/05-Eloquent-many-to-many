af8aff1761a7bc375326cfc1361c200e2a8285da
Himalayan <hdnpress@gmail.com>
2023-02-01 14:29:16 +0545

    Establish the relationship and seed the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Tag::factory(20)->create();
+
+        Article::factory(20)->create()->each(function ($article) {
+            $article->tags()->attach([rand(1, 20), rand(1, 20)]);
+        });
     }
 }
